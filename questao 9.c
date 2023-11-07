b39a12d6174e00a150b3ce2974481f4036ae5875
yenit6 <144127970+yenit6@users.noreply.github.com>
2023-11-07 11:18:49 -0300

    Add files via upload

--- questao 9.c
+++ questao 9.c
@@ -2,17 +2,46 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <string.h>
-#include  <ctype.h>
+#include <ctype.h>
 
-int main(){
+int main()
+{
 
+    int main()
+    {
+        int numero;
+        int i;
+        int flag = 0;
 
+        printf("Digite um numero inteiro: ");
+        scanf("%d", &numero);
 
+        if (numero <= 1)
+        {
+            printf("Numero nao primo\n");
+            return 0;
+        }
 
+        for (i = 2; i <= numero / 2; ++i)
+        {
+            if (numero % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
 
+        switch (flag)
+        {
+        case 0:
+            printf("Numero primo\n");
+            break;
+        default:
+            printf("Numero nao primo\n");
+            break;
+        }
+        return 0;
+    }
 
-
-
-return 0;
-
+    return 0;
 }
