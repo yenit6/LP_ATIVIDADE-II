2b2018c103bca910a4f10a8d3bdf3e9a6083e7f6
yenit6 <144127970+yenit6@users.noreply.github.com>
2023-11-07 11:16:31 -0300

    Add files via upload

---
+++ questao 12.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+    float media, nota;
+    int somaNotas = 0;
+    int i;
+    int qntNotas = 0;
+
+    printf("Digite a quantidade de notas: \n");
+    scanf("%d", &qntNotas);
+
+    for (i = 1; i <= qntNotas; i++)
+    {
+        printf("Digite a %d nota: \n", i);
+        scanf("%f", &nota);
+
+        somaNotas += nota;
+    }
+
+    media = somaNotas / (float)qntNotas;
+
+printf("soma: %d\n", somaNotas);
+    printf("Media: %.2f \n", media);
+
+    return 0;
+}
